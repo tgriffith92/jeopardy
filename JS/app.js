e93217de0103549f3138f7732563f3243b4b7306
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-08-07 12:16:36 -0400

    make all of the numbers move to the correct point values on click

--- JS/app.js
+++ JS/app.js
@@ -159,3 +159,246 @@ const secondOneCont = () => {
 
 //onclick move the number 2 into the second 100 container
 document.querySelector('.secondOne').addEventListener('click', secondOneCont);
+
+//third column
+const thirdFiveCont = () => {
+
+    event.preventDefault();
+
+   let numberFive = document.querySelector('.anime');
+   
+   let thirdFive = document.querySelector('.thirdFive');
+   
+   //make the number 3 a child of the third 500 container
+   thirdFive.appendChild(numberFive);
+
+}
+
+//onclick move the number 3 into the third 500 container
+document.querySelector('.thirdFive').addEventListener('click', thirdFiveCont);
+
+const thirdFourCont = () => {
+
+   event.preventDefault();
+
+  let numberFour = document.querySelector('.anime');
+  
+  let thirdFour = document.querySelector('.thirdFour');
+  
+  //make the number 3 a child of the third 400 container
+  thirdFour.appendChild(numberFour);
+
+}
+
+//onclick move the number 3 into the third 400 container
+document.querySelector('.thirdFour').addEventListener('click', thirdFourCont);
+
+const thirdThreeCont = () => {
+
+   event.preventDefault();
+
+  let numberThree = document.querySelector('.anime');
+  
+  let thirdThree = document.querySelector('.thirdThree');
+  
+  //make the number 3 a child of the third 300 container
+  thirdThree.appendChild(numberThree);
+
+}
+
+//onclick move the number 3 into the third 300 container
+document.querySelector('.thirdThree').addEventListener('click', thirdThreeCont);
+
+const thirdTwoCont = () => {
+
+   event.preventDefault();
+
+  let numberTwo = document.querySelector('.anime');
+  
+  let thirdTwo = document.querySelector('.thirdTwo');
+  
+  //make the number 3 a child of the third 200 container
+  thirdTwo.appendChild(numberTwo);
+
+}
+
+//onclick move the number 3 into the third 200 container
+document.querySelector('.thirdTwo').addEventListener('click', thirdTwoCont);
+
+const thirdOneCont = () => {
+
+   event.preventDefault();
+
+  let numberOne = document.querySelector('.anime');
+  
+  let thirdOne = document.querySelector('.thirdOne');
+  
+  //make the number 3 a child of the third 100 container
+  thirdOne.appendChild(numberOne);
+
+}
+
+//onclick move the number 3 into the third 100 container
+document.querySelector('.thirdOne').addEventListener('click', thirdOneCont);
+
+//fourth column
+const fourthFiveCont = () => {
+
+    event.preventDefault();
+
+   let numberFive = document.querySelector('.basketball');
+   
+   let fourthFive = document.querySelector('.fourthFive');
+   
+   //make the number 4 a child of the third 500 container
+   fourthFive.appendChild(numberFive);
+
+}
+
+//onclick move the number 4 into the third 500 container
+document.querySelector('.fourthFive').addEventListener('click', fourthFiveCont);
+
+const fourthFourCont = () => {
+
+   event.preventDefault();
+
+  let numberFour = document.querySelector('.basketball');
+  
+  let fourthFour = document.querySelector('.fourthFour');
+  
+  //make the number 4 a child of the third 400 container
+  fourthFour.appendChild(numberFour);
+
+}
+
+//onclick move the number 4 into the third 400 container
+document.querySelector('.fourthFour').addEventListener('click', fourthFourCont);
+
+const fourthThreeCont = () => {
+
+   event.preventDefault();
+
+  let numberThree = document.querySelector('.basketball');
+  
+  let fourthThree = document.querySelector('.fourthThree');
+  
+  //make the number 4 a child of the third 300 container
+  fourthThree.appendChild(numberThree);
+
+}
+
+//onclick move the number 4 into the third 300 container
+document.querySelector('.fourthThree').addEventListener('click', fourthThreeCont);
+
+const fourthTwoCont = () => {
+
+   event.preventDefault();
+
+  let numberTwo = document.querySelector('.basketball');
+  
+  let fourthTwo = document.querySelector('.fourthTwo');
+  
+  //make the number 4 a child of the third 200 container
+  fourthTwo.appendChild(numberTwo);
+
+}
+
+//onclick move the number 4 into the third 200 container
+document.querySelector('.fourthTwo').addEventListener('click', fourthTwoCont);
+
+const fourthOneCont = () => {
+
+   event.preventDefault();
+
+  let numberOne = document.querySelector('.basketball');
+  
+  let fourthOne = document.querySelector('.fourthOne');
+  
+  //make the number 4 a child of the third 100 container
+  fourthOne.appendChild(numberOne);
+
+}
+
+//onclick move the number 4 into the third 100 container
+document.querySelector('.fourthOne').addEventListener('click', fourthOneCont);
+
+//fifth column
+const fifthFiveCont = () => {
+
+    event.preventDefault();
+
+   let numberFive = document.querySelector('.marvelMovies');
+   
+   let fifthFive = document.querySelector('.fifthFive');
+   
+   //make the number 5 a child of the third 500 container
+   fifthFive.appendChild(numberFive);
+
+}
+
+//onclick move the number 5 into the third 500 container
+document.querySelector('.fifthFive').addEventListener('click', fifthFiveCont);
+
+const fifthFourCont = () => {
+
+   event.preventDefault();
+
+  let numberFour = document.querySelector('.marvelMovies');
+  
+  let fifthFour = document.querySelector('.fifthFour');
+  
+  //make the number 5 a child of the third 400 container
+  fifthFour.appendChild(numberFour);
+
+}
+
+//onclick move the number 5 into the third 400 container
+document.querySelector('.fifthFour').addEventListener('click', fifthFourCont);
+
+const fifthThreeCont = () => {
+
+   event.preventDefault();
+
+  let numberThree = document.querySelector('.marvelMovies');
+  
+  let fifthThree = document.querySelector('.fifthThree');
+  
+  //make the number 5 a child of the third 300 container
+  fifthThree.appendChild(numberThree);
+
+}
+
+//onclick move the number 5 into the third 300 container
+document.querySelector('.fifthThree').addEventListener('click', fifthThreeCont);
+
+const fifthTwoCont = () => {
+
+   event.preventDefault();
+
+  let numberTwo = document.querySelector('.marvelMovies');
+  
+  let fifthTwo = document.querySelector('.fifthTwo');
+  
+  //make the number 5 a child of the third 200 container
+  fifthTwo.appendChild(numberTwo);
+
+}
+
+//onclick move the number 5 into the third 200 container
+document.querySelector('.fifthTwo').addEventListener('click', fifthTwoCont);
+
+const fifthOneCont = () => {
+
+   event.preventDefault();
+
+  let numberOne = document.querySelector('.marvelMovies');
+  
+  let fifthOne = document.querySelector('.fifthOne');
+  
+  //make the number 5 a child of the third 100 container
+  fifthOne.appendChild(numberOne);
+
+}
+
+//onclick move the number 5 into the third 100 container
+document.querySelector('.fifthOne').addEventListener('click', fifthOneCont);
