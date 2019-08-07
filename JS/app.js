6106b97bb1014cf913afeacdf5a5c4cbbafe0b86
terencegriffithCB5 <terence.griffith92@gmail.com>
2019-08-07 11:28:48 -0400

    move the number 1 to the first 400 container when clicked

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,12 @@
-const addNumber = () => {
+const addNumberFiveOne = () => {
 
-    
      event.preventDefault();
 
-     
     let numberFive = document.querySelector('.football');
     
     let firstFive = document.querySelector('.firstFive');
     
+    //make the number 1 a child of the first 500 container
     firstFive.appendChild(numberFive);
 
     //set the innerText of firstFive equal to numberFive
@@ -16,5 +15,24 @@ const addNumber = () => {
     return firstFive;
 }
 
-    document.querySelector('.firstFive').addEventListener('click', addNumber);
+document.querySelector('.firstFive').addEventListener('click', addNumberFiveOne);
+
+const addNumberFourOne = () => {
+
+    event.preventDefault();
+
+   let numberFour = document.querySelector('.football');
+   
+   let firstFour = document.querySelector('.firstFour');
+   
+   //make the number 1 a child of the first 500 container
+   firstFour.appendChild(numberFour);
+
+   //set the innerText of firstFive equal to numberFive
+   firstFour = numberFour;
+   
+   return firstFour;
+}
+
+document.querySelector('.firstFour').addEventListener('click', addNumberFourOne);
 
